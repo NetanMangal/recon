66d7bbfa6c5260032e58b9f957b49fcb992fb880
NetanMangal <imnetanmangal@gmail.com>
2020-12-17 00:10:44 +0530

    just

--- ultimate_recon.sh
+++ ultimate_recon.sh
@@ -21,6 +21,9 @@ if [ -z "$domain" ]; then
         exit 1
 fi
 
+mkdir $domain
+cd $domain
+
 echo "${BLUE} ######################################################### ${RESET}"
 echo "${BLUE} #                   Finding Subdomains                  # ${RESET}"
 echo "${BLUE} ######################################################### ${RESET}"
@@ -31,15 +34,14 @@ assetfinder --subs-only $domain | tee -a op.txt
 amass enum -passive -d $doamin | tee -a op.txt
 amass enum -active -d $domain -ip | tee -a amass_ips.txt
 cat amass_ips.txt | awk '{print $1}' | tee -a op.txt
-curl -s https://crt.sh/\?q\=$1\&output\=json | jq &>/dev/null | awk -F'"' '/value/{print $4}' | sed 's/*.//g;s/\\n/\n/g' | sed 's/ /\n/g' | tee -a op.txt
-curl -s https://otx.alienvault.com/api/v1/indicators/domain/$1/passive_dns | jq &>/dev/null | awk -F': ' '/"hostname": "(.*'$1')"/{print $2}' | sed 's/[",]//g' | sed 's/ /\n/g' | tee -a op.txt
-curl -s https://dns.bufferover.run/dns\?q\=.$1 | awk -F',' '/.*'$1'/{print $2}' | sed 's/"//g' | sed 's/ /\n/g' | tee -a op.txt
-curl -s http://api.hackertarget.com/hostsearch/\?q\=$1 | cut -d',' -f1 | sed 's/ /\n/g' | tee -a op.txt
-curl -s https://rapiddns.io/subdomain/$1\?full\=1 | awk -F'">' '/'$1'/{print $2}' | cut -d'<' -f1 | awk 'NF' | sed 's/ /\n/g' | tee -a op.txt
-curl -s https://api.sublist3r.com/search.php\?domain\=$1 | jq &>/dev/null | awk -F'"' '/'$1'/{print $2}' | sed 's/ /\n/g' | tee -a op.txt
-curl -s https://www.threatcrowd.org/searchApi/v2/domain/report/\?domain\=$1 | jq &>/dev/null | awk -F'"' '/\.'$1'/{print $2}' | sed 's/ //g' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://crt.sh/\?q\=$domain\&output\=json | jq &>/dev/null | awk -F'"' '/value/{print $4}' | sed 's/*.//g;s/\\n/\n/g' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://otx.alienvault.com/api/v1/indicators/domain/$domain/passive_dns | jq &>/dev/null | awk -F': ' '/"hostname": "(.*'$domain')"/{print $2}' | sed 's/[",]//g' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://dns.bufferover.run/dns\?q\=.$domain | awk -F',' '/.*'$domain'/{print $2}' | sed 's/"//g' | sed 's/ /\n/g' | tee -a op.txt
+curl -s http://api.hackertarget.com/hostsearch/\?q\=$domain | cut -d',' -f1 | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://rapiddns.io/subdomain/$domain\?full\=1 | awk -F'">' '/'$domain'/{print $2}' | cut -d'<' -f1 | awk 'NF' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://api.sublist3r.com/search.php\?domain\=$domain | jq &>/dev/null | awk -F'"' '/'$domain'/{print $2}' | sed 's/ /\n/g' | tee -a op.txt
+curl -s https://www.threatcrowd.org/searchApi/v2/domain/report/\?domain\=$domain | jq &>/dev/null | awk -F'"' '/\.'$domain'/{print $2}' | sed 's/ //g' | sed 's/ /\n/g' | tee -a op.txt
 cat op.txt | sort -u | tee -a all.txt
-
 
 echo "${GREEN} ######################################################### ${RESET}"
 echo "${GREEN} #                Subdomain Bruteforcing                 # ${RESET}"
@@ -50,14 +52,12 @@ mv results_output.txt dns_op.txt
 cat dns_op.txt >output.txt
 cat output.txt | sort -u | tee -a all.txt
 
-
 echo "${BLUE} ######################################################### ${RESET}"
 echo "${BLUE} #              Checking for alive subdomains            # ${RESET}"
 echo "${BLUE} ######################################################### ${RESET}"
 
 cat all.txt | httprobe | tee -a alive2.txt
 cat alive2.txt | sort -u | tee -a alive.txt
-
 
 echo "${GREEN} ######################################################### ${RESET}"
 echo "${GREEN} #                          MassDNS                      # ${RESET}"
@@ -67,7 +67,6 @@ echo "${GREEN} ######################################################### ${RESET
 cat massdns.raw | grep -e ' A ' | cut -d 'A' -f 2 | tr -d ' ' >massdns.txt
 cat *.txt | sort -V | uniq >final-ips.txt
 echo -e "${BLUE}[*] Check the list of IP addresses at final-ips.txt${RESET}"
-
 
 echo "${BLUE} ######################################################### ${RESET}"
 echo "${BLUE} #                       Starting Nuclei                 # ${RESET}"
@@ -82,20 +81,17 @@ nuclei -l alive.txt -t "/root/tools/nuclei-templates/technologies/*.yaml" -c 60 
 nuclei -l alive.txt -t "/root/tools/nuclei-templates/tokens/*.yaml" -c 60 -o nuclei_op/tokens.txt
 nuclei -l alive.txt -t "/root/tools/nuclei-templates/vulnerabilities/*.yaml" -c 60 -o nuclei_op/vulnerabilities.txt
 
-
 echo "${GREEN} ######################################################### ${RESET}"
 echo "${GREEN} #           Looking for CORS misconfiguration           # ${RESET}"
 echo "${GREEN} ######################################################### ${RESET}"
 
 python3 ~/tools/Corsy/corsy.py -i alive.txt -t 40 | tee -a corsy_op.txt
 
-
 echo "${BLUE} ######################################################### ${RESET}"
 echo "${BLUE} #                   Starting CMS detection              # ${RESET}"
 echo "${BLUE} ######################################################### ${RESET}"
 
 whatweb -i alive.txt | tee -a whatweb_op.txt
-
 
 echo "${GREEN} ######################################################### ${RESET}"
 echo "${GREEN} #                          WayBack                      # ${RESET}"
@@ -109,7 +105,6 @@ cat wayback_data/wb.txt | grep -P "\w+\.php(\?|$)" | sort -u | tee -a wayback_da
 cat wayback_data/wb.txt | grep -P "\w+\.aspx(\?|$)" | sort -u | tee -a wayback_data/aspxurls.txt
 cat wayback_data/wb.txt | grep -P "\w+\.jsp(\?|$)" | sort -u | tee -a wayback_data/jspurls.txt
 cat wayback_data/wb.txt | grep -P "\w+\.txt(\?|$)" | sort -u | tee -a wayback_data/robots.txt
-
 
 echo "${BLUE} ######################################################### ${RESET}"
 echo "${BLUE} #            Looking for HTTP request smuggling         # ${RESET}"
@@ -170,9 +165,10 @@ cat alive.txt | smuggler | tee -a smuggler_op.txt
 # jsep
 # cat endpoints/*/* | sort -u | tee -a endpoints.txt
 
-
 echo "${GREEN} ######################################################### ${RESET}"
 echo "${GREEN} #                            FFUF                       # ${RESET}"
 echo "${GREEN} ######################################################### ${RESET}"
 
 for i in $(cat alive.txt); do ffuf -u $i/FUZZ -w ~/tools/dirsearch/db/dicc.txt -mc 200 -t 60; done | tee -a ffuf_op.txt
+
+cd ..

bf5743645450097d9ad5d7380ef4108b29ce6edd
NetanMangal <imnetanmangal@gmail.com>
2021-02-09 14:23:30 +0530

    naabu on all.txt

--- ultimate_recon.sh
+++ ultimate_recon.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ~/.bash_profile
+
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 BLUE=$(tput setaf 4)
@@ -59,6 +61,12 @@ echo "${BLUE} ######################################################### ${RESET}
 cat all.txt | httprobe -c 100 | tee -a alive2.txt
 cat alive2.txt | sort -u | tee -a alive.txt
 
+echo "${BLUE} ######################################################### ${RESET}"
+echo "${BLUE} #                      Running Naabu                    # ${RESET}"
+echo "${BLUE} ######################################################### ${RESET}"
+
+naabu -iL all.txt -c 40 -p - -no-color -o naabu_portscan.txt;
+
 # echo "${GREEN} ######################################################### ${RESET}"
 # echo "${GREEN} #                          MassDNS                      # ${RESET}"
 # echo "${GREEN} ######################################################### ${RESET}"
@@ -75,20 +83,20 @@ echo "${BLUE} #                       Starting Nuclei                 # ${RESET}
 echo "${BLUE} ######################################################### ${RESET}"
 
 mkdir nuclei_op
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/cves/" -c 60 -o nuclei_op/cves.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/default-logins/" -c 60 -o nuclei_op/default-logins.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/dns/" -c 60 -o nuclei_op/dns.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/exposed-panels/" -c 60 -o nuclei_op/exposed-panels.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/exposed-tokens/" -c 60 -o nuclei_op/exposed-tokens.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/exposures/" -c 60 -o nuclei_op/exposures.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/fuzzing/" -c 60 -o nuclei_op/fuzzing.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/helpers/" -c 60 -o nuclei_op/helpers.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/miscellaneous/" -c 60 -o nuclei_op/miscellaneous.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/misconfiguration/" -c 60 -o nuclei_op/misconfiguration.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/takeovers/" -c 60 -o nuclei_op/takeovers.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/technologies/" -c 60 -o nuclei_op/technologies.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/vulnerabilities/" -c 60 -o nuclei_op/vulnerabilities.txt
-nuclei -l alive.txt -t "/root/tools/nuclei-templates/workflows/" -c 60 -o nuclei_op/workflows.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/cves/" -c 100 -o nuclei_op/cves.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/default-logins/" -c 100 -o nuclei_op/default-logins.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/dns/" -c 100 -o nuclei_op/dns.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/exposed-panels/" -c 100 -o nuclei_op/exposed-panels.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/exposed-tokens/" -c 100 -o nuclei_op/exposed-tokens.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/exposures/" -c 100 -o nuclei_op/exposures.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/fuzzing/" -c 100 -o nuclei_op/fuzzing.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/helpers/" -c 100 -o nuclei_op/helpers.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/miscellaneous/" -c 100 -o nuclei_op/miscellaneous.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/misconfiguration/" -c 100 -o nuclei_op/misconfiguration.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/takeovers/" -c 100 -o nuclei_op/takeovers.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/technologies/" -c 100 -o nuclei_op/technologies.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/vulnerabilities/" -c 100 -o nuclei_op/vulnerabilities.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/workflows/" -c 100 -o nuclei_op/workflows.txt
 
 echo "${GREEN} ######################################################### ${RESET}"
 echo "${GREEN} #           Looking for CORS misconfiguration           # ${RESET}"

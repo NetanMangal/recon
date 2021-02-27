e4221dcef56be5e0704ef1a65e13b888ead52aa2
NetanMangal <imnetanmangal@gmail.com>
2021-02-27 22:44:19 +0530

    auditor.sh added v1.0

---
+++ auditor.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+BLUE=$(tput setaf 4)
+RESET=$(tput sgr0)
+
+echo "${RED} ######################################################### ${RESET}"
+echo "${RED} #                         Let's Hunt                    # ${RESET}"
+echo "${RED} ######################################################### ${RESET}"
+
+while getopts ":d:" input; do
+        case "$input" in
+        d)
+                domain=${OPTARG}
+                ;;
+        esac
+done
+if [ -z "$domain" ]; then
+        echo "${BLUE}Please give a domain like \"-d domain.com\"${RESET}"
+        exit 1
+fi
+
+sources='alive.txt alive2.txt all.txt amass_ips.txt corsy_op.txt data_output dns_op.txt massdns.raw naabu_portscan.txt nmap_op nmap_scan.txt nuclei_op op.txt output.txt smuggler_op.txt'
+
+echo "${GREEN} ######################################################### ${RESET}"
+echo "${GREEN} #                         $domain                    # ${RESET}"
+echo "${GREEN} ######################################################### ${RESET}"
+
+for item in $sources; do
+    if [[ $(ls -lah | awk '{print $NF}' | grep -E "$item") -ne "$item" ]]; then
+        echo "${RED} # $item not found # ${RESET}"
+    fi
+done
